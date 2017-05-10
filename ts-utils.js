5536dd24dff3c43f0335f967f2e0a355e58e1e73
Pyry Avist <pyry.avist@gmail.com>
2017-05-10 12:46:40 +0300

    Update ts-utils.js

--- ts-utils.js
+++ ts-utils.js
@@ -52,7 +52,7 @@ function getDeps(sourceFile, checker) {
 
   function isExternal(module) {
     console.log(module)
-    console.log(JSON.stringify(module,null,2)
+    console.log(JSON.stringify(module,null,2))
     var decl = module.declarations[0];
     var sf = decl.getSourceFile();
     return sf.isDeclarationFile;
